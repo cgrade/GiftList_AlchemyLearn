3866bbf1bbf02ee477090d29bfe4b6ff15f24076
cgrade <gradeisageek@gmail.com>
2023-12-10 04:19:40 +0100

    Solution to the Task Provided: Handling the data from the front end

--- server/index.js
+++ server/index.js
@@ -8,14 +8,23 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = 'a1a47fdb2d2171fc16147d4e9b21a78cfefec96299a7f07b00fe22f18020b7af';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof }= req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  /*
+    TODO: prove that a name is in the list
+
+    SOLUTION:
+      - get the parameters from the request body (frontend)
+      - pass these parameters into the verifyProof func from MerkleTree Class
+  */
+  
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+
+  // check and return a response depending on a truety or falsy value.
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
