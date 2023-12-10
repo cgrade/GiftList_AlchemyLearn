f976bc7f187df9f756025765ba18a684b4398945
cgrade <gradeisageek@gmail.com>
2023-12-10 04:18:43 +0100

    Solutions to the task provided

--- client/index.js
+++ client/index.js
@@ -5,10 +5,30 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
 
+  
+  /* -----------------SOLUTION--------------------------- */
+
+  // Instantiating a MerkleTree Class
+  const merkleTree = new MerkleTree(niceList);
+
+  // generating the RootHash of the Tree instantiated above
+  const root = merkleTree._getRoot();
+
+  // name to verify
+  const name = "Erick Glover";
+
+  // index of name above in the niceList
+  const index = niceList.findIndex( n => n === name);
+
+  // Getting the proof form the merkle tree
+  const proof = merkleTree.getProof(index);
+
+// Sending a Post Request to the server.
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name, proof
   });
 
   console.log({ gift });
